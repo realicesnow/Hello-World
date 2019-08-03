54acb1f6662f845119069d5f11aa9cb125c8e591
realicesnow <realicesnow@gmail.com>
2019-08-03 17:27:12 +0900

    L2_BS_1 * 爬取分类目录

--- ReptileLearning/L2_BS_1.py
+++ ReptileLearning/L2_BS_1.py
@@ -7,38 +7,24 @@ import csv
 #引入requests库
 folder = "D:\\GitHub\\OutputData""\\"
 
-def iterative_find_li(result_set, rows):
-    for item in result_set:
-        li_items = item.find_all("li")
-        for li_item in li_items:
-            art_item = li_item.find("article")
-            footer = art_item.find("footer")
-            div_author = footer.find("div", class_="comment-author vcard")
-            author = div_author.find("b")
-            div_time = footer.find("div", class_="comment-metadata")
-            time = div_time.find("time")
-            div_content = art_item.find("div", class_="comment-content")
-            content = div_content.find("p")
-            rows.append( ( author.text, time.text.strip("\n\t"), content.text ) )
-            ol_items = li_item.find_all("ol")
-            iterative_find_li(ol_items, rows)
-
-def analyze_with_bs( text ):
+def analyze_with_bs(text):
     soup = bs(text, "html.parser")
-    ol_items = soup.find_all("ol", class_="comment-list")
+    tag_ul = soup.find("ul", class_="nav nav-list")
+    tag_li = tag_ul.find("li")
+    tags_li = tag_li.find_all("li")
     rows = []
-    iterative_find_li(ol_items, rows)
-    for row in rows:
-        print(row)
+    for li in tags_li:
+        rows.append( (li.find("a").text.strip(),) )
     return rows
 
-
-def training_request_html():
-    text = rs_data('https://wordpress-edu-3autumn.localprod.forc.work/all-about-the-future_04/')
+def training_request_categories():
+    text = rs_data("http://books.toscrape.com/")
     if text == None:
-        return
+        return None
 
     rows = analyze_with_bs(text)
+    print("共计{}类".format(len(rows)))
+    print(rows)
     name = input("请输入保存文件名(输入为空时默认使用内容前五字.csv)：")
     if name == "":
         name=text[:6]
@@ -49,4 +35,4 @@ def training_request_html():
     print("文件已保存到：{}".format(path))
 
 if __name__ == "__main__":
-    training_request_html()   #网页下载
+    training_request_categories()
