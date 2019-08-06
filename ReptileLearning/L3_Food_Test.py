e316a115a4547d76c8e4005505f746ebf5fb5d42
realicesnow <realicesnow@gmail.com>
2019-08-06 22:02:50 +0900

    L3 Test

--- ReptileLearning/L3_Food_Test.py
+++ ReptileLearning/L3_Food_Test.py
@@ -7,6 +7,29 @@ import csv
 #引入requests库
 folder = "D:\\GitHub\\OutputData""\\"
 
+def training_request_list_div():
+    home_url = "http://www.xiachufang.com"
+    week_url = home_url+"/explore/"
+    res = requests.get(week_url)
+    print(res.status_code)
+    soup = bs(res.text,"html.parser")
+    tag_names = soup.find_all("p", class_="name")
+    tag_ellipsis = soup.find_all("p", class_="ing ellipsis")
+    if len(tag_names) != len(tag_ellipsis):
+        print("长度不一致")
+        return None
+    count = 0
+    rows = []
+    while count < len(tag_names):
+        name_a = tag_names[count].find("a")
+        name = name_a.text.strip()
+        url = home_url+name_a["href"]
+        ellipsis = tag_ellipsis[count].text.strip()
+        rows.append((name.strip(), ellipsis, url))
+        print(rows[-1])
+        count += 1
+    return rows
+
 def training_request_list():
     home_url = "http://www.xiachufang.com"
     week_url = home_url+"/explore/"
@@ -36,7 +59,7 @@ def training_request_foods():
     rows = training_request_list()
     name = input("请输入保存文件名(输入为空时默认使用内容前五字.csv)：")
     if name == "":
-        name=rows[0][0][:6]
+        name=rows[0][0][:5]
     path = folder+name+".csv"
     with open(path, "w", newline='', encoding="utf-8-sig") as f:
         writer = csv.writer(f)
